0394ef57e8d72b4c365c22a9df70cd4e6ff43dfd
ajax333221 <myemailisepic@gmail.com>
2020-09-03 22:34:43 -0500

    ui v1.6.0

--- js/isepic-chess-ui.js
+++ js/isepic-chess-ui.js
@@ -4,7 +4,7 @@
 
 (function(windw, $, Ic){
 	var IcUi=(function(){
-		var _VERSION="1.5.8";
+		var _VERSION="1.6.0";
 		
 		var _ANIMATE_DURATION=300;
 		
@@ -47,6 +47,33 @@
 			});
 		}
 		
+		function _bindOnce(){
+			var temp;
+			
+			temp=$("#ic_id_fen");
+			
+			if(temp.length && !temp.attr("data-binded")){
+				temp.attr("data-binded", "1");
+				
+				temp.click(function(){
+					$(this).select();
+				});
+			}
+			
+			temp=$("#ic_id_debug_toggle");
+			
+			if(temp.length && !temp.attr("data-binded")){
+				temp.attr("data-binded", "1");
+				
+				temp.click(function(){
+					$(this).text("Debug "+($("#ic_id_objinfo").is(":visible") ? "▲" : "▼"));
+					$("#ic_id_objinfo").toggle();
+					
+					return false;
+				});
+			}
+		}
+		
 		function _reBindBoardLinks(){
 			$(".ic_changeboard").unbind("click").click(function(){
 				var board, board_name, no_errors;
@@ -79,79 +106,58 @@
 		function _refreshBoardTabs(board_name){
 			var i, len, current_board, current_board_name, board_list, new_html;
 			
-			board_list=Ic.getBoardNames();
-			new_html="<strong>Board list:</strong> ";
-			
-			for(i=0, len=board_list.length; i<len; i++){//0<len
-				new_html+=(i ? " | " : "");
-				current_board_name=board_list[i];
-				current_board=Ic.selectBoard(current_board_name);
+			if($("#ic_id_tabs").length){
+				board_list=Ic.getBoardNames();
+				new_html="<strong>Board list:</strong> ";
 				
-				if(Ic.boardExists(current_board)){
-					if(current_board.isHidden){
-						new_html+="<em class='ic_disabled'>"+current_board_name+"</em>";
-					}else if(current_board_name===board_name){
-						new_html+="<em>"+current_board_name+"</em>";
+				for(i=0, len=board_list.length; i<len; i++){//0<len
+					new_html+=(i ? " | " : "");
+					current_board_name=board_list[i];
+					current_board=Ic.selectBoard(current_board_name);
+					
+					if(Ic.boardExists(current_board)){
+						if(current_board.isHidden){
+							new_html+="<em class='ic_disabled'>"+current_board_name+"</em>";
+						}else if(current_board_name===board_name){
+							new_html+="<em>"+current_board_name+"</em>";
+						}else{
+							new_html+="<a class='ic_changeboard' data-boardname='"+current_board_name+"' href='#'>"+current_board_name+"</a>";
+						}
 					}else{
-						new_html+="<a class='ic_changeboard' data-boardname='"+current_board_name+"' href='#'>"+current_board_name+"</a>";
+						Ic.utilityMisc.consoleLog("Warning[_refreshBoardTabs]: \""+current_board_name+"\" is not defined");
 					}
-				}else{
-					Ic.utilityMisc.consoleLog("Warning[_refreshBoardTabs]: \""+current_board_name+"\" is not defined");
 				}
+				
+				$("#ic_id_tabs").html(new_html);
 			}
-			
-			$("#ic_id_tabs").html(new_html);
 		}
 		
 		function _refreshTable(is_rotated){
 			var i, j, rank_bos, current_bos, new_html;
 			
-			new_html="<table class='"+("ic_tableb"+(is_rotated ? " ic_rotated" : ""))+"' cellpadding='0' cellspacing='0'>";
-			new_html+="<tr><td class='ic_label ic_top_border ic_left_border'></td><td class='ic_label ic_top_border'>"+(is_rotated ? "hgfedcba" : "abcdefgh").split("").join("</td><td class='ic_label ic_top_border'>")+"</td><td class='"+("ic_label ic_top_border ic_right_border ic_dot "+(is_rotated ? "ic_wside" : "ic_bside"))+"'>◘</td><td class='ic_captureds' rowspan='10'></td></tr>";
-			
-			for(i=0; i<8; i++){//0...7
-				rank_bos=(is_rotated ? (i+1) : (8-i));
+			if($("#ic_id_board").length){
+				new_html="<table cellpadding='0' cellspacing='0'>";
+				new_html+="<tr><td class='ic_label ic_top_border ic_left_border'></td><td class='ic_label ic_top_border'>"+(is_rotated ? "hgfedcba" : "abcdefgh").split("").join("</td><td class='ic_label ic_top_border'>")+"</td><td class='"+("ic_label ic_top_border ic_right_border ic_dot "+(is_rotated ? "ic_wside" : "ic_bside"))+"'>◘</td><td class='ic_captureds' rowspan='10'></td></tr>";
 				
-				new_html+="<tr><td class='ic_label ic_left_border'>"+rank_bos+"</td>";
-				
-				for(j=0; j<8; j++){//0...7
-					current_bos=Ic.toBos(is_rotated ? [(7-i), (7-j)] : [i, j]);
+				for(i=0; i<8; i++){//0...7
+					rank_bos=(is_rotated ? (i+1) : (8-i));
 					
-					new_html+="<td id='"+("ic_id_"+current_bos)+"' class='"+((i+j)%2 ? "ic_bs" : "ic_ws")+"' data-bos='"+current_bos+"'><div class='ic_piece_holder'></div></td>";
+					new_html+="<tr><td class='ic_label ic_left_border'>"+rank_bos+"</td>";
+					
+					for(j=0; j<8; j++){//0...7
+						current_bos=Ic.toBos(is_rotated ? [(7-i), (7-j)] : [i, j]);
+						
+						new_html+="<td id='"+("ic_id_"+current_bos)+"' class='"+((i+j)%2 ? "ic_bs" : "ic_ws")+"' data-bos='"+current_bos+"'><div class='ic_piece_holder'></div></td>";
+					}
+					
+					new_html+="<td class='ic_label ic_right_border'>"+rank_bos+"</td></tr>";
 				}
 				
-				new_html+="<td class='ic_label ic_right_border'>"+rank_bos+"</td></tr>";
+				new_html+="<tr><td class='ic_label ic_bottom_border ic_left_border'></td><td class='ic_label ic_bottom_border'>"+(is_rotated ? "hgfedcba" : "abcdefgh").split("").join("</td><td class='ic_label ic_bottom_border'>")+"</td><td class='"+("ic_label ic_right_border ic_bottom_border ic_dot "+(is_rotated ? "ic_bside" : "ic_wside"))+"'>◘</td></tr>";
+				new_html+="</table>";
+				
+				$("#ic_id_board").attr("class", (is_rotated ? "ic_rotated" : "")).html(new_html);
 			}
-			
-			new_html+="<tr><td class='ic_label ic_bottom_border ic_left_border'></td><td class='ic_label ic_bottom_border'>"+(is_rotated ? "hgfedcba" : "abcdefgh").split("").join("</td><td class='ic_label ic_bottom_border'>")+"</td><td class='"+("ic_label ic_right_border ic_bottom_border ic_dot "+(is_rotated ? "ic_bside" : "ic_wside"))+"'>◘</td></tr>";
-			new_html+="</table>";
-			
-			$("#ic_id_board").html(new_html);
-		}
-		
-		function _appendOnceToBody(){
-			var rtn_appended;
-			
-			rtn_appended=false;
-			
-			if(!$("#ic_id_main").length){
-				rtn_appended=true;
-				
-				$("body").append("<div id='ic_id_main'><h3 class='ic_inlineb'>GitHub: <a href='https://github.com/ajax333221/isepic-chess'>isepic-chess</a> | <a href='https://github.com/ajax333221/isepic-chess-ui'>isepic-chess-ui</a></h3><div id='ic_id_board'></div><div id='ic_id_controls'><input id='ic_id_fen' value='' type='text'><br><input id='ic_id_nav_first' value='|<' type='button'> <input id='ic_id_nav_previous' value='<' type='button'> <input id='ic_id_nav_next' value='>' type='button'> <input id='ic_id_nav_last' value='>|' type='button'><input id='ic_id_rotate' value='rotate' type='button'><select id='ic_id_promote'><option value='5' selected='selected'>queen</option><option value='4'>rook</option><option value='3'>bishop</option><option value='2'>knight</option></select><hr><p id='ic_id_tabs'></p><p id='ic_id_movelist'></p></div><div id='ic_id_infoholder'><a id='ic_id_debug_toggle' href='#'>Debug ▲</a><ul id='ic_id_objinfo' style='display:none'></ul></div></div>");
-				
-				$("#ic_id_fen").click(function(){
-					$(this).select();
-				});
-				
-				$("#ic_id_debug_toggle").click(function(){
-					$(this).text("Debug "+($("#ic_id_objinfo").is(":visible") ? "▲" : "▼"));
-					$("#ic_id_objinfo").toggle();
-					
-					return false;
-				});
-			}
-			
-			return rtn_appended;
 		}
 		
 		//---------------- utilities (this=apply)
@@ -305,15 +311,17 @@
 			
 			that=this;
 			
-			for(i=0; i<8; i++){//0...7
-				for(j=0; j<8; j++){//0...7
-					reset_class=((i+j)%2 ? "ic_bs" : "ic_ws");
-					current_square=that.getSquare(that.isRotated ? [(7-i), (7-j)] : [i, j]);
-					
-					square_class=current_square.className;
-					square_class=(square_class ? (" ic_"+square_class) : "");
-					
-					$("#ic_id_"+current_square.bos).attr("class", reset_class).html("<div class='"+("ic_piece_holder"+square_class)+"'></div>");
+			if($("#ic_id_board").length){
+				for(i=0; i<8; i++){//0...7
+					for(j=0; j<8; j++){//0...7
+						reset_class=((i+j)%2 ? "ic_bs" : "ic_ws");
+						current_square=that.getSquare(that.isRotated ? [(7-i), (7-j)] : [i, j]);
+						
+						square_class=current_square.className;
+						square_class=(square_class ? (" ic_"+square_class) : "");
+						
+						$("#ic_id_"+current_square.bos).attr("class", reset_class).html("<div class='"+("ic_piece_holder"+square_class)+"'></div>");
+					}
 				}
 			}
 		}
@@ -323,18 +331,20 @@
 			
 			that=this;
 			
-			captured_html="";
-			
-			for(i=0; i<2; i++){//0...1
-				current_side=(that.isRotated===!i ? that.materialDiff.w : that.materialDiff.b);
-				captured_html+=(i ? "<hr>" : "");
+			if($("#ic_id_board .ic_captureds").length){
+				captured_html="";
 				
-				for(j=0, len=current_side.length; j<len; j++){//0<len
-					captured_html+="<img src='"+("./css/images/"+Ic.toClassName(current_side[j])+".png")+"' width='20' height='20'>";
+				for(i=0; i<2; i++){//0...1
+					current_side=(that.isRotated===!i ? that.materialDiff.w : that.materialDiff.b);
+					captured_html+=(i ? "<hr>" : "");
+					
+					for(j=0, len=current_side.length; j<len; j++){//0<len
+						captured_html+="<img src='"+("./css/images/"+Ic.toClassName(current_side[j])+".png")+"' width='20' height='20'>";
+					}
 				}
+				
+				$("#ic_id_board .ic_captureds").html(captured_html);
 			}
-			
-			$("#ic_id_board .ic_captureds").html(captured_html);
 		}
 		
 		function _refreshMoveList(){
@@ -342,24 +352,26 @@
 			
 			that=this;
 			
-			move_list=that.moveList;
-			black_starts=Ic.utilityMisc.strContains(move_list[0].Fen, " b ");
-			initial_full_move=(that.fullMove-Math.floor((that.currentMove+black_starts-1)/2)+(black_starts===!(that.currentMove%2))-1);
-			
-			new_html="";
-			
-			for(i=1, len=move_list.length; i<len; i++){//1<len
-				new_html+=(i!==1 ? " " : "");
-				new_html+=(black_starts===!(i%2) ? ("<span class='ic_pgn_number'>"+(initial_full_move+Math.floor((i+black_starts-1)/2))+".</span>") : "");
-				new_html+="<span class='"+(i!==that.currentMove ? "ic_pgn_link" : "ic_pgn_current")+"' data-index='"+i+"'>"+move_list[i].PGNmove+"</span>";
-				new_html+=(move_list[i].PGNend ? (" <span class='ic_pgn_result'>"+move_list[i].PGNend+"</span>") : "");
+			if($("#ic_id_movelist").length){
+				move_list=that.moveList;
+				black_starts=Ic.utilityMisc.strContains(move_list[0].Fen, " b ");
+				initial_full_move=(that.fullMove-Math.floor((that.currentMove+black_starts-1)/2)+(black_starts===!(that.currentMove%2))-1);
+				
+				new_html="";
+				
+				for(i=1, len=move_list.length; i<len; i++){//1<len
+					new_html+=(i!==1 ? " " : "");
+					new_html+=(black_starts===!(i%2) ? ("<span class='ic_pgn_number'>"+(initial_full_move+Math.floor((i+black_starts-1)/2))+".</span>") : "");
+					new_html+="<span class='"+(i!==that.currentMove ? "ic_pgn_link" : "ic_pgn_current")+"' data-index='"+i+"'>"+move_list[i].PGNmove+"</span>";
+					new_html+=(move_list[i].PGNend ? (" <span class='ic_pgn_result'>"+move_list[i].PGNend+"</span>") : "");
+				}
+				
+				if(black_starts && new_html){
+					new_html="<span class='ic_pgn_number'>"+initial_full_move+"...</span>"+new_html;
+				}
+				
+				$("#ic_id_movelist").html(new_html || "...");
 			}
-			
-			if(black_starts && new_html){
-				new_html="<span class='ic_pgn_number'>"+initial_full_move+"...</span>"+new_html;
-			}
-			
-			$("#ic_id_movelist").html(new_html || "...");
 		}
 		
 		function _refreshDebug(){
@@ -367,72 +379,74 @@
 			
 			that=this;
 			
-			new_html="<li><strong>Selected board:</strong> <span>"+that.boardName+"</span></li>";
-			new_html+="<li><strong>Is rotated?:</strong> <span>"+that.isRotated+"</span></li>";
-			new_html+="<li><strong>Number of checks:</strong> <span>"+that.checks+"</span></li>";
-			new_html+="<li><strong>Is check?:</strong> <span>"+that.isCheck+"</span></li>";
-			new_html+="<li><strong>Is checkmate?:</strong> <span>"+that.isCheckmate+"</span></li>";
-			new_html+="<li><strong>Is stalemate?:</strong> <span>"+that.isStalemate+"</span></li>";
-			new_html+="<li><strong>Is threefold repetition?:</strong> <span>"+that.isThreefold+"</span></li>";
-			new_html+="<li><strong>Is fifty-move rule?:</strong> <span>"+that.isFiftyMove+"</span></li>";
-			new_html+="<li><strong>Is insufficient material?:</strong> <span>"+that.isInsufficientMaterial+"</span></li>";
-			new_html+="<li><strong>In draw?:</strong> <span>"+that.inDraw+"</span></li>";
-			new_html+="<li><strong>En Passant square:</strong> <span>"+(that.enPassantBos || "-")+"</span></li>";
-			new_html+="<li><strong>Active color:</strong> <span>"+that.activeColor+"</span></li>";
-			new_html+="<li><strong>Non active color:</strong> <span>"+that.nonActiveColor+"</span></li>";
-			
-			new_html+="<li>";
-			new_html+="<strong>W</strong>";
-			new_html+="<ul>";
-			new_html+="<li><strong>king square:</strong> <span>"+that.w.kingBos+"</span></li>";
-			new_html+="<li><strong>castling rights:</strong> <span>"+(Ic.utilityMisc.castlingChars(that.w.castling).toUpperCase() || "-")+"</span></li>";
-			new_html+="</ul>";
-			new_html+="</li>";
-			
-			new_html+="<li>";
-			new_html+="<strong>B</strong>";
-			new_html+="<ul>";
-			new_html+="<li><strong>king square:</strong> <span>"+that.b.kingBos+"</span></li>";
-			new_html+="<li><strong>castling rights:</strong> <span>"+(Ic.utilityMisc.castlingChars(that.b.castling) || "-")+"</span></li>";
-			new_html+="</ul>";
-			new_html+="</li>";
-			
-			new_html+="<li><strong>Half moves:</strong> <span>"+that.halfMove+"</span></li>";
-			new_html+="<li><strong>Full moves:</strong> <span>"+that.fullMove+"</span></li>";
-			new_html+="<li><strong>Current move:</strong> <span>"+that.currentMove+"</span></li>";
-			new_html+="<li><strong>Promote to:</strong> <span>"+Ic.toBal(that.promoteTo*that[that.activeColor].sign)+"</span></li>";
-			new_html+="<li><strong>Selected square:</strong> <span>"+(that.selectedBos || "-")+"</span></li>";
-			new_html+="<li><strong>Material difference:</strong> <span>{w:["+that.materialDiff.w.join(", ")+"], b:["+that.materialDiff.b.join(", ")+"]}</span></li>";
-			
-			new_html+="<li>";
-			new_html+="<strong>Squares</strong>";
-			new_html+="<ul>";
-			
-			for(i=0; i<8; i++){//0...7
-				current_row=[];
+			if($("#ic_id_objinfo").length){
+				new_html="<li><strong>Selected board:</strong> <span>"+that.boardName+"</span></li>";
+				new_html+="<li><strong>Is rotated?:</strong> <span>"+that.isRotated+"</span></li>";
+				new_html+="<li><strong>Number of checks:</strong> <span>"+that.checks+"</span></li>";
+				new_html+="<li><strong>Is check?:</strong> <span>"+that.isCheck+"</span></li>";
+				new_html+="<li><strong>Is checkmate?:</strong> <span>"+that.isCheckmate+"</span></li>";
+				new_html+="<li><strong>Is stalemate?:</strong> <span>"+that.isStalemate+"</span></li>";
+				new_html+="<li><strong>Is threefold repetition?:</strong> <span>"+that.isThreefold+"</span></li>";
+				new_html+="<li><strong>Is fifty-move rule?:</strong> <span>"+that.isFiftyMove+"</span></li>";
+				new_html+="<li><strong>Is insufficient material?:</strong> <span>"+that.isInsufficientMaterial+"</span></li>";
+				new_html+="<li><strong>In draw?:</strong> <span>"+that.inDraw+"</span></li>";
+				new_html+="<li><strong>En Passant square:</strong> <span>"+(that.enPassantBos || "-")+"</span></li>";
+				new_html+="<li><strong>Active color:</strong> <span>"+that.activeColor+"</span></li>";
+				new_html+="<li><strong>Non active color:</strong> <span>"+that.nonActiveColor+"</span></li>";
 				
-				for(j=0; j<8; j++){//0...7
-					current_square=that.getSquare([i, j]);
+				new_html+="<li>";
+				new_html+="<strong>W</strong>";
+				new_html+="<ul>";
+				new_html+="<li><strong>king square:</strong> <span>"+that.w.kingBos+"</span></li>";
+				new_html+="<li><strong>castling rights:</strong> <span>"+(Ic.utilityMisc.castlingChars(that.w.castling).toUpperCase() || "-")+"</span></li>";
+				new_html+="</ul>";
+				new_html+="</li>";
+				
+				new_html+="<li>";
+				new_html+="<strong>B</strong>";
+				new_html+="<ul>";
+				new_html+="<li><strong>king square:</strong> <span>"+that.b.kingBos+"</span></li>";
+				new_html+="<li><strong>castling rights:</strong> <span>"+(Ic.utilityMisc.castlingChars(that.b.castling) || "-")+"</span></li>";
+				new_html+="</ul>";
+				new_html+="</li>";
+				
+				new_html+="<li><strong>Half moves:</strong> <span>"+that.halfMove+"</span></li>";
+				new_html+="<li><strong>Full moves:</strong> <span>"+that.fullMove+"</span></li>";
+				new_html+="<li><strong>Current move:</strong> <span>"+that.currentMove+"</span></li>";
+				new_html+="<li><strong>Promote to:</strong> <span>"+Ic.toBal(that.promoteTo*that[that.activeColor].sign)+"</span></li>";
+				new_html+="<li><strong>Selected square:</strong> <span>"+(that.selectedBos || "-")+"</span></li>";
+				new_html+="<li><strong>Material difference:</strong> <span>{w:["+that.materialDiff.w.join(", ")+"], b:["+that.materialDiff.b.join(", ")+"]}</span></li>";
+				
+				new_html+="<li>";
+				new_html+="<strong>Squares</strong>";
+				new_html+="<ul>";
+				
+				for(i=0; i<8; i++){//0...7
+					current_row=[];
 					
-					temp=""+current_square.val;
-					
-					if(temp.length===1){
-						temp=" "+temp;
+					for(j=0; j<8; j++){//0...7
+						current_square=that.getSquare([i, j]);
+						
+						temp=""+current_square.val;
+						
+						if(temp.length===1){
+							temp=" "+temp;
+						}
+						
+						current_row.push("<span title='"+(current_square.bos.toUpperCase()+" = "+(current_square.className || "empty"))+"'>"+temp+"</span>");
 					}
 					
-					current_row.push("<span title='"+(current_square.bos.toUpperCase()+" = "+(current_square.className || "empty"))+"'>"+temp+"</span>");
+					new_html+="<li><strong>A"+(8-i)+"-H"+(8-i)+":</strong> "+current_row.join(" | ")+"</li>";
 				}
 				
-				new_html+="<li><strong>A"+(8-i)+"-H"+(8-i)+":</strong> "+current_row.join(" | ")+"</li>";
+				new_html+="</ul>";
+				new_html+="</li>";
+				
+				new_html+="<li><strong>FEN:</strong> <span>"+that.fen+"</span></li>";
+				new_html+="<li><strong>Version:</strong> <span>[Ic_v"+Ic.version+"] [IcUi_v"+_VERSION+"]</span></li>";
+				
+				$("#ic_id_objinfo").html(new_html);
 			}
-			
-			new_html+="</ul>";
-			new_html+="</li>";
-			
-			new_html+="<li><strong>FEN:</strong> <span>"+that.fen+"</span></li>";
-			new_html+="<li><strong>Version:</strong> <span>[Ic_v"+Ic.version+"] [IcUi_v"+_VERSION+"]</span></li>";
-			
-			$("#ic_id_objinfo").html(new_html);
 		}
 		
 		//---------------- ic ui (this=apply)
@@ -478,7 +492,7 @@
 		}
 		
 		function refreshBoard(animation_type){
-			var that, just_appended;
+			var that;
 			
 			that=this;
 			
@@ -487,9 +501,9 @@
 				
 				that.selectedBos="";
 				
-				just_appended=_appendOnceToBody();
+				_bindOnce();
 				
-				if(just_appended || $("#ic_id_board .ic_tableb").hasClass("ic_rotated")!==that.isRotated){
+				if(!$("#ic_id_board").html() || $("#ic_id_board").hasClass("ic_rotated")!==that.isRotated){
 					_refreshTable(that.isRotated);
 				}
 				
@@ -514,15 +528,17 @@
 				_refreshMoveList.apply(that, []);
 				_reBindPgnLinks.apply(that, []);
 				
-				_refreshActiveDot(that[that.activeColor].isBlack);
-				
-				if(animation_type){
-					_animateCaller.apply(that, [animation_type<0]);
-				}
-				
-				if(that.currentMove!==0){
-					$("#ic_id_"+that.moveList[that.currentMove].FromBos).addClass("ic_lastmove");
-					$("#ic_id_"+that.moveList[that.currentMove].ToBos).addClass("ic_lastmove");
+				if($("#ic_id_board").length){
+					_refreshActiveDot(that[that.activeColor].isBlack);
+					
+					if(animation_type){
+						_animateCaller.apply(that, [animation_type<0]);
+					}
+					
+					if(that.currentMove!==0){
+						$("#ic_id_"+that.moveList[that.currentMove].FromBos).addClass("ic_lastmove");
+						$("#ic_id_"+that.moveList[that.currentMove].ToBos).addClass("ic_lastmove");
+					}
 				}
 			}
 		}
